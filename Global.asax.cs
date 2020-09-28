3131373ff1a44ae0b39fa6f056caeb35ab3f0ff5
Donma <no2don@gmail.com>
2020-09-28 14:48:32 +0800

    edit

--- Global.asax.cs
+++ Global.asax.cs
@@ -19,7 +19,7 @@ namespace GithubImageLite
 
         public readonly static string ServerToken = "pass";
         public readonly static string ImageNotFound = "https://raw.githubusercontent.com/ctimggit/storage1/master/404.png";
-        public readonly static decimal  PerRepoLimit =400;
+        public readonly static decimal  PerRepoLimit =699;
         public static bool IsWriting { get; set; }
         protected void Application_Start(object sender, EventArgs e)
         {
